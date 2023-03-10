acbca7359dc540ec94c4e0f3d2366d2d25e55964
davidecaputo <daca9621@gmail.com>
2023-03-10 19:30:12 +0100

    Add square

--- js/script.js
+++ js/script.js
@@ -6,14 +6,28 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 //Creare un ciclo for per creare i numeri da 1 a 100
 
+const container = document.querySelector('.container');
+
 for(i = 1; i <= 100; i++){
     if(i % 3 === 0 && i % 5 === 0){
         console.log('FizzBuzz');
+        container.innerHTML += `
+            <div class="square square-green text-white d-flex justify-content-center align-items-center fs-5">FizzBuzz</div>
+        `
     } else if(i % 3 === 0) {
         console.log('Fizz');
+        container.innerHTML += `
+            <div class="square text-white square-red d-flex justify-content-center align-items-center fs-5">Fizz</div>
+        `
     } else if(i % 5 === 0) {
         console.log('Buzz');
+        container.innerHTML += `
+            <div class="square text-white square-orange d-flex justify-content-center align-items-center fs-5">Buzz</div>
+        `
     } else {
         console.log(i);
+        container.innerHTML += `
+            <div class="square text-white d-flex justify-content-center align-items-center fs-5">${i}</div>
+        `
     }
 };
